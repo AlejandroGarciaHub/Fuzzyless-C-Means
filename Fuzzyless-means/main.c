0a576103c05b167af95cd645dae084f56df0f82b
Miguel Garcia <miguel_itecit@hotmail.com>
2018-01-09 12:33:45 -0600

    Initial Commit

---
+++ Fuzzyless-means/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Fuzzyless-means
+//
+//  Created by Miguel Garcia on 1/9/18.
+//  Copyright © 2018 Miguel Garcia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
